354543043ac80e93c93de05952f61df624b1a607
mehedi hasan <mdchanchal5675@gmail.com>
2024-05-16 06:25:32 +0600

    hei

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
-const app: Application = express();
+import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
 //parsers
-app.use(express.json());
-app.use(cors());
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+app.use(express.json())
+app.use(cors())
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
 
-export default app;
+export default app
